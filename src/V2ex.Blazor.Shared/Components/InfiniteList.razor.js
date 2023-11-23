e3d75e490a6558cdca9d9d28100218c773468726
rwecho <rwecho@live.com>
2023-11-23 18:56:30 +0800

    add InfiniteList

---
+++ src/V2ex.Blazor.Shared/Components/InfiniteList.razor.js
@@ -0,0 +1,54 @@
+﻿function throttle(callback, limit) {
+    let waiting = false;
+    return function () {
+        if (!waiting) {
+            callback.apply(this, arguments);
+            waiting = true;
+            setTimeout(function () {
+                waiting = false;
+            }, limit);
+        } else {
+            console.log("throttle");
+        }
+    }
+}
+
+export function initialize(dotNetHelper) {
+    const container = document.querySelector("#infinite-list");
+    if (!container) {
+        return
+    }
+
+    const loadMoreThrottle = throttle(() => {
+        dotNetHelper.invokeMethodAsync("LoadMore");
+    }, 300);
+
+    // listen the container vertical scroll change event
+    container.addEventListener("scroll", function (e) {
+        const target = e.target;
+        const scrollTop = target.scrollTop;
+        const scrollHeight = target.scrollHeight;
+        const clientHeight = target.clientHeight;
+        const offset = 5;
+
+        // if the scroll bar is close to the bottom, load more content
+        const heightToBottom = scrollHeight - scrollTop - clientHeight;
+        if (heightToBottom < offset) {
+            console.log("load more", heightToBottom);
+            loadMoreThrottle();
+        }
+    });
+}
+
+export function ensureLoadComplete(dotNetHelper) {
+    const container = document.querySelector("#infinite-list");
+    if (!container) {
+        return
+    }
+
+    // if the scroll bar is not show, load more content
+    if (container.scrollHeight <= container.clientHeight) {
+        console.log("the scroll bar is not show, load more");
+        dotNetHelper.invokeMethodAsync("LoadMore");
+    }
+}
