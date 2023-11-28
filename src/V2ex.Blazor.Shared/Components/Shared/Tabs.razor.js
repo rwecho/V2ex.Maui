62236e4325e8ac19cfc33e68664c6befb79139ae
rwecho <rwecho@live.com>
2023-11-28 18:29:46 +0800

    fix home with multiple tab instance.

--- src/V2ex.Blazor.Shared/Components/Shared/Tabs.razor.js
+++
@@ -1,8 +0,0 @@
-export function initializeSwiper() {
-    const swiper = new Swiper('.swiper-tabs', {
-        slidesPerView: 5,
-        spaceBetween: 0,
-        freeMode: true
-    });
-    console.log("initialize swiper.");
-}
