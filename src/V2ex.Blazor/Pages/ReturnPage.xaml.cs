201e2eb8906a8a7f5a363e52a91db55dfd08f53b
rwecho <rwecho@live.com>
2023-12-01 10:16:23 +0800

    set default index page to ReturnPage

--- src/V2ex.Blazor/Pages/ReturnPage.xaml.cs
+++ src/V2ex.Blazor/Pages/ReturnPage.xaml.cs
@@ -4,11 +4,34 @@ public partial class ReturnPage : ContentPage
 {
     public ReturnPage(string targetLocation)
     {
+        this.TargetLocation = targetLocation;
         InitializeComponent();
 
         RootComponent.Parameters = new Dictionary<string, object?>
         {
             { "Url", targetLocation }
         };
+    }
+
+    public string TargetLocation
+    {
+        get;
+        set;
+    }
+
+    public bool HasNavigationBar
+    {
+        get
+        {
+            return DeviceInfo.Platform != DevicePlatform.WinUI;
+        }
+    }
+
+    public string PageTitle
+    {
+        get
+        {
+            return "";
+        }
     }
 }
