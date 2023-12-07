ef9f3fb76501e810bea9ce60916e2884d608f645
rwecho <rwecho@live.com>
2023-12-07 16:37:13 +0800

    add PullToRefresh.js

---
+++ src/V2ex.Blazor.Shared/Components/RefreshView.razor.js
@@ -0,0 +1,15 @@
+﻿export function initialize(mainElementId, thisRef) {
+   return PullToRefresh.init({
+       mainElement: "#" + mainElementId,
+       triggerElement: "#" + mainElementId,
+       onRefresh() {
+            thisRef.invokeMethodAsync('OnRefreshAsync');
+        },
+    })
+}
+
+export function destroy(instance) {
+    if (instance) {
+        instance.destroy();
+    }
+}
