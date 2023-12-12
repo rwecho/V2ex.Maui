b380a4df0a0bea17bd53f46f168c12afac1936de
rwecho <rwecho@live.com>
2023-12-12 12:04:04 +0800

    add swiper to HomePage

--- src/V2ex.Blazor.Shared/Pages/Home.razor.js
+++ src/V2ex.Blazor.Shared/Pages/Home.razor.js
@@ -1,1 +1,38 @@
-﻿
+﻿export function initialize(thisRef, initialSlide) {
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: false,
+        initialSlide: initialSlide,
+
+        touchStartPreventDefault: false,
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+        on: {
+            init: function () {
+                if (initialSlide == 0) {
+                    thisRef.invokeMethodAsync("OnSlideChangeAsync", 0);
+                }
+            },
+            slideChange: function () {
+                thisRef.invokeMethodAsync("OnSlideChangeAsync", this.activeIndex);
+            },
+        },
+    });
+
+    return swiper;
+}
