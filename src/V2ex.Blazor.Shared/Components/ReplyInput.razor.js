42eb95f16478afaa3726678c61f4f2fbdfa2455e
rwecho <rwecho@live.com>
2023-12-18 18:51:34 +0800

    add reply input.

---
+++ src/V2ex.Blazor.Shared/Components/ReplyInput.razor.js
@@ -0,0 +1,113 @@
+﻿const imgurClientIdPool = [
+  "3107b9ef8b316f3",
+  "442b04f26eefc8a",
+  "59cfebe717c09e4",
+  "60605aad4a62882",
+  "6c65ab1d3f5452a",
+  "83e123737849aa9",
+  "9311f6be1c10160",
+  "c4a4a563f698595",
+  "81be04b9e4a08ce",
+];
+function imageHandler() {
+  //prompt user to choose a local image and uploading it to imgur then insert the image url to the editor
+  const quill = this.quill;
+}
+
+function emojiHandler() {
+  const quill = this.quill;
+  const range = quill.getSelection(true);
+
+  quill.insertEmbed(range.index, "image", "https://i.imgur.com/io2SM1h.png");
+}
+
+export function initialize(containerRef, thisRef) {
+  const editorDiv = containerRef.querySelector("#editor");
+  var SizeStyle = Quill.import("attributors/style/size");
+  Quill.register(SizeStyle, true);
+  var quill = new Quill(editorDiv, {
+    modules: {
+      toolbar: false,
+    },
+    theme: "snow",
+  });
+  return quill;
+}
+
+export function insertEmoji(quill, emoji) {
+  const range = quill.getSelection(true);
+  quill.insertEmbed(range.index, "text", emoji);
+
+  const nextIndex = range.index + emoji.length;
+  quill.setSelection(nextIndex, Quill.sources.SILENT);
+}
+
+export function insertClassicEmoji(quill, emojiImageUrl) {
+  const range = quill.getSelection(true);
+  quill.insertEmbed(range.index, "image", emojiImageUrl);
+
+  const nextIndex = range.index + emojiImageUrl.length;
+  quill.setSelection(nextIndex, Quill.sources.SILENT);
+}
+
+export async function chooseImage(quill, thisRef) {
+  const input = document.createElement("input");
+  const promise = new Promise((resolve, reject) => {
+    input.setAttribute("type", "file");
+    input.setAttribute("accept", "image/png, image/gif, image/jpeg");
+    input.classList.add("quill-image-input", "hidden");
+    document.body.appendChild(input);
+
+    input.addEventListener("cancel", function () {
+        reject("cancel");
+    });
+
+    input.addEventListener("change", async function () {
+      if (!input.files || !input.files[0]) {
+        reject();
+        return;
+      }
+      const file = input.files[0];
+      const range = quill.getSelection(true);
+
+      // reset the input value;
+      input.value = "";
+
+      const formData = new FormData();
+      formData.append("image", file);
+
+      //upload image to imgur
+      const randomIndex = Math.floor(Math.random() * imgurClientIdPool.length);
+      const clidenId = imgurClientIdPool[randomIndex];
+
+      try {
+        const response = await fetch("https://api.imgur.com/3/upload", {
+          method: "POST",
+          headers: { Authorization: `Client-ID ${clidenId}` },
+          body: formData,
+        });
+
+        if (response.ok) {
+          const resData = await response.json();
+          if (resData.success) {
+            //insert image url to editor
+            quill.insertEmbed(range.index, "image", resData.data.link);
+            resolve();
+            return;
+          }
+        }
+        reject();
+      } catch (e) {
+        reject(e.message);
+      }
+    });
+
+    input.click();
+  });
+
+  try {
+    return await promise;
+  } finally {
+    document.body.removeChild(input);
+  }
+}
--- src/V2ex.Blazor.Shared/Pages/Test.razor.js
+++ src/V2ex.Blazor.Shared/Pages/Test.razor.js
@@ -1,91 +1,29 @@
-﻿const imgurClientIdPool = [
-  "3107b9ef8b316f3",
-  "442b04f26eefc8a",
-  "59cfebe717c09e4",
-  "60605aad4a62882",
-  "6c65ab1d3f5452a",
-  "83e123737849aa9",
-  "9311f6be1c10160",
-  "c4a4a563f698595",
-  "81be04b9e4a08ce",
-];
-function imageHandler() {
-  //prompt user to choose a local image and uploading it to imgur then insert the image url to the editor
-  const quill = this.quill;
-  let input = document.querySelector(".quill-image-input");
-  if (input == null) {
-    input = document.createElement("input");
-    input.setAttribute("type", "file");
-    input.setAttribute("accept", "image/png, image/gif, image/jpeg");
-    input.classList.add("quill-image-input", "hidden");
-    document.body.appendChild(input);
-  }
-
-  input.addEventListener("change", async function () {
-    if (!input.files || !input.files[0]) {
-      return;
-    }
-    const file = input.files[0];
-    const range = quill.getSelection(true);
-
-    const formData = new FormData();
-    formData.append("image", file);
-
-    //upload image to imgur
-    const randomIndex = Math.floor(Math.random() * imgurClientIdPool.length);
-    const clidenId = imgurClientIdPool[randomIndex];
-    const response = await fetch("https://api.imgur.com/3/upload", {
-      method: "POST",
-      headers: { Authorization: `Client-ID ${clidenId}` },
-      body: formData,
-    });
-
-    input.value = "";
-
-    if (response.ok) {
-      const resData = await response.json();
-      if (resData.success) {
-        //insert image url to editor
-        quill.insertEmbed(range.index, "image", resData.data.link);
-        alert("上传成功");
-        return;
-      }
-    }
-
-    alert("上传失败");
-  });
-
-  input.click();
-}
-
-function emojiHandler() {
-  const quill = this.quill;
-    const range = quill.getSelection(true);
-
-    quill.insertEmbed(range.index, "image", "https://i.imgur.com/io2SM1h.png");
-}
-
-export function initialize() {
-  var quill = new Quill("#editor", {
-    modules: {
-      toolbar: {
-        container: [
-          ["image"], //add image here
-              ["emoji"],
-
-        ],
-        handlers: {
-          image: imageHandler, //Add it here
-          emoji: emojiHandler,
+﻿export function initializeModal(containerRef, thisRef) {
+    const $modalElement = containerRef.querySelector("#replyInputModal");
+    const modalOptions = {
+        placement: 'bottom-center',
+        backdrop: "dynamic",
+        backdropClasses:
+            'bg-gray-900/50 dark:bg-gray-900/80 fixed inset-0 z-40',
+        closable: true,
+        onHide: () => {
+            console.log('modal is hidden');
+            document.body.classList.add('overflow-hidden');
+            //thisRef.invokeMethodAsync('OnModalHideJsInvoke');
         },
-      },
-    },
-    theme: "snow",
-  });
+        onShow: () => {
+            console.log('modal is shown');
+            //thisRef.invokeMethodAsync('OnModalShowJsInvoke');
+        },
+        onToggle: () => {
+            console.log('modal has been toggled');
+        },
+    };
 
-  return quill;
-}
+    const instanceOptions = {
+        id: 'replyInputModal',
+        override: true
+    };
 
-export function test(quill) {
-  alert(quill);
-}
+    return new Modal($modalElement, modalOptions, instanceOptions);
+};
