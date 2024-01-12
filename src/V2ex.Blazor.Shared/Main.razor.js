a6718a91b0877c020f0b483440134e0b8ea34bc0
rwecho <rwecho@live.com>
2024-01-12 17:59:56 +0800

    add platform plugin to tailwindcss.

--- src/V2ex.Blazor.Shared/Main.razor.js
+++ src/V2ex.Blazor.Shared/Main.razor.js
@@ -1,1 +1,9 @@
-﻿
+﻿export function initialize(platform) {
+    if (platform === "unknow") {
+        return;
+    }
+
+    if (!document.documentElement.classList.contains(platform)) {
+        document.documentElement.classList.add(platform);
+    }
+}
--- src/V2ex.Blazor.Shared/tailwind.config.js
+++ src/V2ex.Blazor.Shared/tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   content: ["../**/*.{razor,cshtml,html}", "./node_modules/flowbite/**/*.js"],
   darkMode: "class",
   theme: {
     extend: {},
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [
+    require("flowbite/plugin"),
+    plugin(function ({ addVariant }) {
+      addVariant('ios', `:is(.ios &)`)
+      addVariant('android', `:is(.android &)`)
+      addVariant('windows', `:is(.windows &)`)
+    })
+  ],
   safelist: ["bg-gray-900/50", "list-decimal", "list-disc", "overscroll-contain"],
 };
